7a2cd1285871dd1f80ea4478f8afd4cffdeeeded
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-03-10 00:29:34 -0400

    v2.0

--- README.md
+++ README.md
@@ -8,5 +8,10 @@ ccbins
 ```
 
 ## Change Log
+### v.02 - 3.9.2020
+* Update boot scripts for recently added binaries
+* Add mkshrc stuff
+* Add rest of mod to repo - everything should be self-updating now
+
 ### v1.0 - 02.05.2020
 * Initial Release
--- customize.sh
+++ customize.sh
@@ -16,22 +16,6 @@ alias wget="$MODPATH/busybox-$ARCH32 wget"
 
 test_connection || { ui_print " "; abort "!This mod requires internet for install!"; }
 
-if [ -f $NVBASE/modules/$MODID/system/bin/ccbins ]; then
-  ui_print "- Using current ccbin files/settings"
-  cp -af $NVBASE/modules/$MODID/system $MODPATH
-  cp -pf $NVBASE/modules/$MODID/.* $MODPATH 2>/dev/null
-else
-  mkdir -p $MODPATH/system/bin
-fi
-ui_print "- Downloading needed files"
-wget -O $MODPATH/mod-util.sh https://github.com/Zackptg5/Cross-Compiled-Binaries-Android/raw/master/mod-util.sh 2>/dev/null
-wget -O $MODPATH/system/bin/ccbins https://github.com/Zackptg5/Cross-Compiled-Binaries-Android/raw/master/ccbins 2>/dev/null
-wget -O $MODPATH/busybox https://github.com/Zackptg5/Cross-Compiled-Binaries-Android/raw/master/busybox/busybox-$ARCH 2>/dev/null
-rm -f $MODPATH/busybox-*
-set_perm $MODPATH/busybox 0 0 0755
-set_perm $MODPATH/system/bin/ccbins 0 0 0755
-locs="$(grep '^locs=' $MODPATH/system/bin/ccbins)"
-eval $locs
-for i in $locs; do
-  [ -d $MODPATH$i ] && chmod -R 0755 $MODPATH$i
-done
+[ -f $NVBASE/modules/$MODID/system/bin/ccbins ] && branch="$(grep_prop branch $NVBASE/modules/$MODID/system/bin/ccbins)" || branch=master
+wget -qO $MODPATH/install.sh https://github.com/Zackptg5/Cross-Compiled-Binaries-Android/raw/$branch/ccbins_files/install.sh 2>/dev/null
+. $MODPATH/install.sh
